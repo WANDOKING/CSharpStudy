bc48f02a193de3079d38cf19dca383b96568120f
WANDOKING <dowankim57@gmail.com>
2024-06-24 02:42:27 +0900

    BreakStack, ThrowCorrectly 추가
    예외를 잘못 던져 StackTrace가 날아가는 경우

--- TestSyntax/TestException.cs
+++ TestSyntax/TestException.cs
@@ -25,5 +25,87 @@
                 num2++;
             }
         }
+
+        [TestMethod]
+        public void BreakStackTrace()
+        {
+            try
+            {
+                Method1();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(ex.StackTrace!.Contains("Method1"));
+                Assert.IsFalse(ex.StackTrace!.Contains("Method2"));
+                Assert.IsFalse(ex.StackTrace!.Contains("Method3"));
+            }
+
+            static void Method1()
+            {
+                try
+                {
+                    Method2();
+                }
+                catch (Exception ex)
+                {
+#pragma warning disable CA2200 // Rethrow to preserve stack details
+                    throw ex; // 스택 정보가 날아간다
+#pragma warning restore CA2200 // Rethrow to preserve stack details
+                }
+            }
+
+            static void Method2()
+            {
+                Method3();
+            }
+
+            static void Method3()
+            {
+                int num1 = 10;
+                int num2 = 0;
+                int num3 = num1 / num2;
+                Console.WriteLine(num3);
+            }
+        }
+
+        [TestMethod]
+        public void throwCorrectly()
+        {
+            try
+            {
+                Method1();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(ex.StackTrace!.Contains("Method1"));
+                Assert.IsTrue(ex.StackTrace!.Contains("Method2"));
+                Assert.IsTrue(ex.StackTrace!.Contains("Method3"));
+            }
+
+            static void Method1()
+            {
+                try
+                {
+                    Method2();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+
+            static void Method2()
+            {
+                Method3();
+            }
+
+            static void Method3()
+            {
+                int num1 = 10;
+                int num2 = 0;
+                int num3 = num1 / num2;
+                Console.WriteLine(num3);
+            }
+        }
     }
 }
