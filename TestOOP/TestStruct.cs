a52206b58619793741510e4e5e81c08e62d53ef5
WANDOKING <dowankim57@gmail.com>
2024-06-23 19:53:20 +0900

    TestStruct 추가

---
+++ TestOOP/TestStruct.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestOOP
+{
+    [TestClass]
+    public class TestStruct
+    {
+        public struct Vector2D
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+
+        [TestMethod]
+        public void DefaultConstructor()
+        {
+            Vector2D vector = new Vector2D();
+
+            Assert.AreEqual(0, vector.X);
+            Assert.AreEqual(0, vector.Y);
+        }
+    }
+}
