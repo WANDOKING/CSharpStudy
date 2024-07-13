645a93c12a516e8641f735cdf2ad9dfcb6dbb86e
WANDOKING <dowankim57@gmail.com>
2024-07-14 07:47:51 +0900

    TestBitConverter 추가

---
+++ TestBCL/TestBitConverter.cs
@@ -0,0 +1,39 @@
+﻿namespace TestBCL;
+
+[TestClass]
+public class TestBitConverter
+{
+    [TestMethod]
+    public void ByteOrder()
+    {
+        // 일반적인 시스템이라면 이 값은 true
+        Assert.AreEqual(true, BitConverter.IsLittleEndian);
+    }
+
+    [TestMethod]
+    public void SerializeAndDeserializeBool()
+    {
+        bool valueToSerialize = true;
+        byte[] serialized = BitConverter.GetBytes(valueToSerialize);
+        Assert.AreEqual(sizeof(bool), serialized.Length);
+        Assert.AreEqual(valueToSerialize, BitConverter.ToBoolean(serialized));
+        Assert.AreEqual("01", BitConverter.ToString(serialized));
+    }
+
+    [TestMethod]
+    public void SerializeAndDeserializeInt()
+    {
+        int valueToSerialize = 10;
+        byte[] serialized = BitConverter.GetBytes(valueToSerialize);
+        Assert.AreEqual(sizeof(int), serialized.Length);
+        Assert.AreEqual(valueToSerialize, BitConverter.ToInt32(serialized));
+        Assert.AreEqual("0A-00-00-00", BitConverter.ToString(serialized));
+    }
+
+    [TestMethod]
+    public void DeserializeFail()
+    {
+        byte[] bytes = new byte[2];
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => BitConverter.ToInt32(bytes));
+    }
+}
