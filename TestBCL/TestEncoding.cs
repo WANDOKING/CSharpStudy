591cb05f9099f4c0cf946044252d2db9962ee1bc
WANDOKING <dowankim57@gmail.com>
2024-07-14 07:57:14 +0900

    MixedString, GetByteCount 테스트 추가

--- TestBCL/TestEncoding.cs
+++ TestBCL/TestEncoding.cs
@@ -5,22 +5,24 @@ namespace TestBCL;
 [TestClass]
 public class TestEncoding
 {
+    private const string stringEnglish = "ABCDE";
+    private const string stringKorean = "가나다라";
+    private const string stringMixed = "A가B나C다D라";
+
     [TestMethod]
     public void SerializeAndDeserializeEnglish()
     {
-        string str = "ABCDE";
-        byte[] serialized = Encoding.UTF8.GetBytes(str);
-        Assert.AreEqual(1 * str.Length, serialized.Length);
-        Assert.AreEqual(str, Encoding.UTF8.GetString(serialized));
+        byte[] serialized = Encoding.UTF8.GetBytes(stringEnglish);
+        Assert.AreEqual(1 * stringEnglish.Length, serialized.Length);
+        Assert.AreEqual(stringEnglish, Encoding.UTF8.GetString(serialized));
         Assert.AreEqual("41-42-43-44-45", BitConverter.ToString(serialized));
     }
 
     [TestMethod]
     public void DeserializePartialEnglish()
     {
-        string str = "ABCDE";
-        byte[] serialized = Encoding.UTF8.GetBytes(str);
-        Assert.AreEqual(1 * str.Length, serialized.Length);
+        byte[] serialized = Encoding.UTF8.GetBytes(stringEnglish);
+        Assert.AreEqual(1 * stringEnglish.Length, serialized.Length);
         Assert.AreEqual("ABC", Encoding.UTF8.GetString(serialized, 0, 3));
         Assert.AreEqual("41-42-43", BitConverter.ToString(serialized, 0, 3));
     }
@@ -28,18 +30,30 @@ public class TestEncoding
     [TestMethod]
     public void SerializeAndDeserializeKorean()
     {
-        string str = "가나다라";
-        byte[] serialized = Encoding.UTF8.GetBytes(str);
-        Assert.AreEqual(3 * str.Length, serialized.Length);
-        Assert.AreEqual(str, Encoding.UTF8.GetString(serialized));
+        byte[] serialized = Encoding.UTF8.GetBytes(stringKorean);
+        Assert.AreEqual(3 * stringKorean.Length, serialized.Length);
+        Assert.AreEqual(stringKorean, Encoding.UTF8.GetString(serialized));
     }
 
     [TestMethod]
     public void DeserializePartialKorean()
     {
-        string str = "가나다라";
-        byte[] serialized = Encoding.UTF8.GetBytes(str);
-        Assert.AreEqual(3 * str.Length, serialized.Length);
+        byte[] serialized = Encoding.UTF8.GetBytes(stringKorean);
+        Assert.AreEqual(3 * stringKorean.Length, serialized.Length);
         Assert.AreEqual("가", Encoding.UTF8.GetString(serialized, 0, 3));
+    }
+
+    [TestMethod]
+    public void SerializeAndDeserializeMixed()
+    {
+        byte[] serialized = Encoding.UTF8.GetBytes(stringMixed);
+        Assert.AreEqual(stringMixed, Encoding.UTF8.GetString(serialized));
+    }
+
+    [TestMethod]
+    public void UTF8ByteCount()
+    {
+        Assert.AreEqual(stringEnglish.Length, Encoding.UTF8.GetByteCount(stringEnglish));
+        Assert.AreEqual(3 * stringKorean.Length, Encoding.UTF8.GetByteCount(stringKorean));
     }
 }
