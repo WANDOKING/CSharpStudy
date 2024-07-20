96e904985fb5f08d5daf65cd834c3f5ab7c0b8fd
WANDOKING <dowankim57@gmail.com>
2024-07-21 00:34:11 +0900

    Environment

---
+++ TestBCL/TestEnvironment.cs
@@ -0,0 +1,23 @@
+﻿namespace TestBCL;
+
+[TestClass]
+public class TestEnvironment
+{
+    [TestMethod]
+    public void ProcessPath()
+    {
+        StringAssert.Contains(Environment.ProcessPath, "TestBCL");
+    }
+
+    [TestMethod]
+    public void SystemPageSize()
+    {
+        Assert.AreEqual(4096, Environment.SystemPageSize);
+    }
+
+    [TestMethod]
+    public void StackTrace()
+    {
+        StringAssert.Contains(Environment.StackTrace, "StackTrace");
+    }
+}
