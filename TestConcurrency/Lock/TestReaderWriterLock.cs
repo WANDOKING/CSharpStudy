27c2ade12f4fc056eef770960be3f175be1281c2
WANDOKING <dowankim57@gmail.com>
2024-08-20 00:15:42 +0900

    TestReaderWriterLock 추가

---
+++ TestConcurrency/Lock/TestReaderWriterLock.cs
@@ -0,0 +1,42 @@
+﻿namespace TestConcurrency.Lock;
+
+[TestClass]
+public class TestReaderWriterLock
+{
+    [TestMethod]
+    public void ReadLock()
+    {
+        ReaderWriterLockSlim rwLock = new ReaderWriterLockSlim();
+
+        Assert.AreEqual(0, rwLock.CurrentReadCount);
+        Assert.IsFalse(rwLock.IsReadLockHeld);
+        Assert.IsFalse(rwLock.IsWriteLockHeld);
+
+        rwLock.EnterReadLock();
+        Assert.AreEqual(1, rwLock.CurrentReadCount);
+        Assert.IsTrue(rwLock.IsReadLockHeld);
+
+        rwLock.ExitReadLock();
+        Assert.AreEqual(0, rwLock.CurrentReadCount);
+        Assert.IsFalse(rwLock.IsReadLockHeld);
+        Assert.IsFalse(rwLock.IsWriteLockHeld);
+    }
+
+
+    [TestMethod]
+    public void WriteLock()
+    {
+        ReaderWriterLockSlim rwLock = new ReaderWriterLockSlim();
+
+        Assert.IsFalse(rwLock.IsReadLockHeld);
+        Assert.IsFalse(rwLock.IsWriteLockHeld);
+
+        rwLock.EnterWriteLock();
+        Assert.IsFalse(rwLock.IsReadLockHeld);
+        Assert.IsTrue(rwLock.IsWriteLockHeld);
+
+        rwLock.ExitWriteLock();
+        Assert.IsFalse(rwLock.IsReadLockHeld);
+        Assert.IsFalse(rwLock.IsWriteLockHeld);
+    }
+}
