b39bdda57084b468eca9b1b4d8feda3169a58c47
WANDOKING <dowankim57@gmail.com>
2024-09-20 02:50:14 +0900

    MessagePack 스터디 내용 추가

--- MessagePackStudy/Program.cs
+++ MessagePackStudy/Program.cs
@@ -7,6 +7,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        // Union 애트리뷰트가 붙은 타입으로 직렬화를 해야 한다.
         IContentsHeader message = new IntsMessage()
         {
             Value0 = 10000,
@@ -14,17 +15,29 @@ internal class Program
             Value2 = 30000,
         };
 
-        var option = MessagePackSerializerOptions.Standard.WithCompression(MessagePackCompression.None);
-
-        var serialized = MessagePackSerializer.Serialize(message, option);
-
+        // 직렬화
+        var serialized = MessagePackSerializer.Serialize(message);
         Console.WriteLine($"{nameof(serialized)}.Length = {serialized.Length}");
         Console.WriteLine($"{BitConverter.ToString(serialized)}");
 
+        // 역직렬화
+        Console.WriteLine("\n[Deserialize]");
         var deserialized = MessagePackSerializer.Deserialize<IContentsHeader>(serialized);
+        switch (deserialized)
+        {
+            case TextMessage textMessage:
+                Console.WriteLine($"{nameof(TextMessage)}: Text = {textMessage.Text}");
+                break;
 
-        var toMessage = deserialized as IntsMessage;
+            case ImageMessage imageMessage:
+                Console.WriteLine($"{nameof(ImageMessage)}: Image = {imageMessage.Image}");
+                break;
 
-        Console.WriteLine($"{toMessage.Value0} | {toMessage.Value1} | {toMessage.Value2}");
+            case IntsMessage intsMessage:
+                Console.WriteLine($"{nameof(IntsMessage)}: {intsMessage.Value0} | {intsMessage.Value1} | {intsMessage.Value2}");
+                break;
+            default:
+                throw new InvalidDataException($"Invalid Message = {deserialized}");
+        }
     }
 }
