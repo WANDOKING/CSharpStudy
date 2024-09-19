51e7f3a544f05577afe047a2074b5f6e4e971c12
WANDOKING <dowankim57@gmail.com>
2024-09-20 02:44:30 +0900

    Temp 프로젝트 초기화

--- Temp/Program.cs
+++ Temp/Program.cs
@@ -1,17 +1,9 @@
-﻿using System.Net;
-using System.Text;
-
-namespace Temp;
+﻿namespace Temp;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        HttpClient client = new HttpClient();
-        string text = client.GetStringAsync("http://www.naver.com:80").Result;
-        Console.WriteLine(text);
-
-        WebClient wc = new WebClient();
-        string 
+        Console.WriteLine("Hello World");
     }
 }
