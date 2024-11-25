9fb6b16d92262858275e91462d8eb7ce963072c6
WANDOKING <dowankim57@gmail.com>
2024-11-26 01:53:57 +0900

    BenchmarkInsert 추가

---
+++ BenchmarkCollection/BenchmarkInsert.cs
@@ -0,0 +1,53 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace BenchmarkCollection;
+
+[MemoryDiagnoser]
+[MaxColumn, MinColumn]
+public class BenchmarkInsert
+{
+    private Stack<int> intStack = new();
+    private Queue<int> intQueue = new();
+    private List<int> intList = new();
+
+    [Params(0, 1000, 10000)]
+    public int InitialCapacity { get; set; }
+
+    [Params(100, 1000, 10000)]
+    public int ElementCount { get; set; }
+
+    [IterationSetup]
+    public void IterationSetup()
+    {
+        intStack = new Stack<int>(InitialCapacity);
+        intQueue = new Queue<int>(InitialCapacity);
+        intList = new List<int>(InitialCapacity);
+    }
+
+    [Benchmark]
+    public void Stack()
+    {
+        for (int i = 0; i < ElementCount; ++i)
+        {
+            intStack.Push(i);
+        }
+    }
+
+    [Benchmark]
+    public void Queue()
+    {
+        for (int i = 0; i < ElementCount; ++i)
+        {
+            intQueue.Enqueue(i);
+        }
+    }
+
+    [Benchmark]
+    public void List()
+    {
+        for (int i = 0; i < ElementCount; ++i)
+        {
+            intList.Add(i);
+        }
+    }
+}
--- BenchmarkCollection/Runner.cs
+++ BenchmarkCollection/Runner.cs
@@ -4,44 +4,10 @@ using System.Text;
 
 namespace BenchmarkCollection;
 
-internal class Program
+internal class Runner
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
-        var summary = BenchmarkRunner.Run<BenchmarkCollection>();
-    }
-}
-
-[MemoryDiagnoser]
-public class BenchmarkCollection
-{
-    private const int loopCount = 1000;
-
-    private Stack<int> intStack = new(loopCount);
-    private Queue<int> intQueue = new(loopCount);
-
-    [IterationSetup]
-    public void IterationSetup()
-    {
-        intStack.Clear();
-        intQueue.Clear();
-    }
-
-    [Benchmark]
-    public void StackPush()
-    {
-        for (int i = 0; i < loopCount; ++i)
-        {
-            intStack.Push(i);
-        }
-    }
-
-    [Benchmark]
-    public void QueueEnqueue()
-    {
-        for (int i = 0; i < loopCount; ++i)
-        {
-            intQueue.Enqueue(i);
-        }
+        BenchmarkSwitcher.FromAssembly(typeof(Runner).Assembly).Run(args);
     }
 }
