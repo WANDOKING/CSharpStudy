c5a5ad189f277fb07a17f3553f1f38ee20a5621f
WANDOKING <dowankim57@gmail.com>
2025-02-10 04:52:47 +0900

    CommandLineParser 추가

---
+++ CommandLineParserStudy/CommandLineOptions.cs
@@ -0,0 +1,15 @@
+﻿using CommandLine;
+
+namespace CommandLineParserStudy;
+
+internal sealed class CommandLineOptions
+{
+    [Value(0)]
+    public string Value0 { get; set; } = string.Empty;
+
+    [Value(1)]
+    public string Value1 { get; set; } = string.Empty;
+
+    [Option("option", HelpText = "option")]
+    public string Option { get; set; } = string.Empty;
+}
---
+++ CommandLineParserStudy/Program.cs
@@ -0,0 +1,38 @@
+﻿using CommandLine;
+
+namespace CommandLineParserStudy;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Parser.Default.ParseArguments<CommandLineOptions>(args)
+            .MapResult(
+                options => Run(options),
+                errors => HandleParseError(errors));
+    }
+
+    private static int HandleParseError(IEnumerable<Error> errors)
+    {
+        Console.WriteLine($"{nameof(HandleParseError)}");
+
+        foreach (var error in errors)
+        {
+            Console.WriteLine(error);
+        }
+
+        return 0;
+    }
+
+    private static int Run(CommandLineOptions options)
+    {
+        Console.WriteLine($"[{nameof(CommandLineOptions)}]");
+
+        foreach (var property in options.GetType().GetProperties())
+        {
+            Console.WriteLine($"{property.Name}: {property.GetValue(options)}");
+        }
+
+        return 0;
+    }
+}
